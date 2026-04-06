c61016adb9959ad6b5670fc200d48c4dc306df4d
vipulkumar7 <vipul15kumar@gmail.com>
2026-04-07 00:06:33 +0530

    margin on close button

--- components/pwa/InstallPrompt.tsx
+++ components/pwa/InstallPrompt.tsx
@@ -93,7 +93,7 @@ export default function InstallPrompt() {
                         {/* CLOSE */}
                         <button
                             onClick={handleClose}
-                            className="absolute top-2 right-2 text-gray-300 hover:text-white"
+                            className="absolute top-2 right-2 text-gray-300 hover:text-white mb-4"
                         >
                             <FiX size={16} />
                         </button>
