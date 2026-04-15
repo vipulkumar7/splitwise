c787a16883c9f8bca55c297a604e00dad5fd7243
vipulkumar7 <vipul15kumar@gmail.com>
2026-04-15 20:28:14 +0530

    minor css fix

--- app/(dashboard)/groups/[id]/page.tsx
+++ app/(dashboard)/groups/[id]/page.tsx
@@ -175,7 +175,7 @@ export default function GroupDetailPage() {
   // UI
   // =========================
   return (
-    <div className="max-w-md mx-auto p-4 h-screen flex flex-col overflow-hidden bg-zinc-900 text-white">
+    <div className="max-w-2xl mx-auto p-4 h-screen flex flex-col overflow-hidden bg-zinc-950 text-white">
       {/* HEADER */}
       <GroupHeader
         groupName={group.name}
